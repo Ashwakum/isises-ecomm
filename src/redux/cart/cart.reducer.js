2464c8f94a682ebdb0bd55107cf495126a2fa397
Ashwini Kumar <aliaadmin@wtwngrok.onmicrosoft.com>
2020-09-01 13:27:44 +0530

    Add item to cart item with quantity as well as cart item added to list

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from './cart.types';
-
+import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,12 +13,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
-                hidden: !state.hidden    
+                hidden: !state.hidden
 
             };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
         default:
-    return state;    
-        
+            return state;
+
     }
 };
 
